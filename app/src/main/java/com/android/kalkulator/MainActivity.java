1fe271cb067d3dd886b1d53139b1028bcf9df5aa
dyanzzz <dyanzzz@gmail.com>
2019-11-30 08:44:06 +0700

    modifikasi hitung rumus dan perbantingkan bilangan

--- app/src/main/java/com/android/kalkulator/MainActivity.java
+++ app/src/main/java/com/android/kalkulator/MainActivity.java
@@ -2,6 +2,7 @@ package com.android.kalkulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -15,7 +16,7 @@ import java.util.Objects;
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_hasil;
-    Button btn_reset;
+    Button btn_reset, btn_hitung_rumus, btn_bandingkan_bilangan;
     EditText edt_bil1, edt_bil2;
     String operasi = "";
 
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         tv_hasil = findViewById(R.id.tv_hasil);
         btn_reset = findViewById(R.id.btn_reset);
+        btn_hitung_rumus = findViewById(R.id.btn_hitung_rumus);
+        btn_bandingkan_bilangan = findViewById(R.id.btn_bandingkan_bilangan);
         edt_bil1 = findViewById(R.id.edt_bil1);
         edt_bil2 = findViewById(R.id.edt_bil2);
 
@@ -35,6 +38,53 @@ public class MainActivity extends AppCompatActivity {
                 edt_bil1.setText("");
                 edt_bil2.setText("");
                 tv_hasil.setText("");
+            }
+        });
+
+        btn_hitung_rumus.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(edt_bil1.getText())) {
+                    edt_bil1.setError("Bilangan 1 Diperlukan");
+                }else if (TextUtils.isEmpty(edt_bil2.getText())) {
+                    edt_bil2.setError("Bilangan 2 Diperlukan");
+                }else {
+                    Double bil1Value = Double.valueOf(edt_bil1.getText().toString());
+                    Double bil2Value = Double.valueOf(edt_bil2.getText().toString());
+                    double hasil = (bil1Value * bil2Value) / 2;
+                    DecimalFormat format = new DecimalFormat("0.#");
+                    String res = "(" + format.format(bil1Value) + " * " + format.format(bil2Value) + ")/2" + " = " + format.format(hasil);
+                    tv_hasil.setText(res);
+                }
+            }
+        });
+
+        btn_bandingkan_bilangan.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(edt_bil1.getText())) {
+                    edt_bil1.setError("Bilangan 1 Diperlukan");
+                }else if (TextUtils.isEmpty(edt_bil2.getText())) {
+                    edt_bil2.setError("Bilangan 2 Diperlukan");
+                }else {
+                    Double bil1Value = Double.valueOf(edt_bil1.getText().toString());
+                    Double bil2Value = Double.valueOf(edt_bil2.getText().toString());
+
+                    String res;
+                    DecimalFormat format = new DecimalFormat("0.#");
+
+                    if (bil1Value > bil2Value) {
+                        res = format.format(bil1Value) + " > " + format.format(bil2Value);
+                    } else if (bil1Value < bil2Value) {
+                        res = format.format(bil1Value) + " < " + format.format(bil2Value);
+                    } else {
+                        res = format.format(bil1Value) + " = " + format.format(bil2Value);
+                    }
+
+                    tv_hasil.setText(res);
+                }
             }
         });
     }
